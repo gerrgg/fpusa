8a1258120659b9b28e0df0c59dfca25a85877e0b
gregbast1994 <gregbast1994@gmail.com>
2019-02-28 16:06:31 -0500

    Update product-specifications.php

--- woocommerce/single-product/product-specifications.php
+++ woocommerce/single-product/product-specifications.php
@@ -5,7 +5,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
-// get product specifications
+$spec_name = json_decode( get_post_meta( get_the_ID(), 'spec_name', true ) );
+$spec_value = json_decode( get_post_meta( get_the_ID(), 'spec_value', true ) );
 
 ?>
 <h2>Specifications Table</h2>
+<table class="table table-hover border" style="width: 100%">
+	<?php for($i = 0; $i < sizeof( $spec_name ); $i++ ) : ?>
+		<?php if( isset( $spec_name[$i], $spec_value[$i] ) ) : ?>
+			<tr>
+				<th><?php echo $spec_name[$i] ?>:</th>
+				<td><?php echo $spec_value[$i] ?></td>
+			</tr>
+		<?php endif; ?>
+	<?php endfor; ?>
+</table>
