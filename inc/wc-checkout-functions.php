8c1799366f2c44a37a57b34b53565f3e44ade3ec
gregbast1994 <gregbast1994@gmail.com>
2019-04-03 10:34:30 -0400

    remove cart-totals.php from cart page

--- inc/wc-checkout-functions.php
+++ inc/wc-checkout-functions.php
@@ -5,11 +5,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 // remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
 // remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
-// remove_action( 'woocommerce_checkout_order_review', 'woocommerce_order_review', 10 );
+remove_action( 'woocommerce_after_cart_table', 'woocommerce_cart_totals' );
+add_action( 'woocommerce_after_cart_table', 'fpusa_get_checkout_button' );
+
+function fpusa_get_checkout_button(){
+	$subtotal = WC()->cart->get_subtotal();
+	$item_cou
+	?>
+	<div class="cart_totals">
+		<div class="card">
+			<div class="card-body">
+				<h5><?php fpusa_get_cart_subtotal(); ?></h5>
+				<a href="<?php echo wc_get_checkout_url(); ?>" role="button" class="btn btn-block btn-warning">Proceed to checkout</a>
+			</div>
+		</div>
+	</div>
+	<?php
+}
 
 add_action( 'fpusa_checkout_step_2', 'woocommerce_checkout_payment', 10 );
 add_action( 'fpusa_after_payment', 'woocommerce_checkout_coupon_form' );
-
 
 add_action( 'fpusa_checkout_step_3', 'fpusa_maybe_set_order_prefs_callback', 10 );
 add_action( 'fpusa_checkout_step_3', 'woocommerce_order_review', 20 );
@@ -19,6 +34,53 @@ function fpusa_checkout_payment(){
 	wc_get_template( '/myaccount/payment-methods.php' );
 }
 
+add_action( 'wp_ajax_fpusa_get_cart_totals', 'fpusa_get_cart_totals' );
+add_action( 'wp_ajax_nopriv_fpusa_get_cart_totals', 'fpusa_get_cart_totals' );
+
+function fpusa_get_cart_totals(){
+	WC()->cart->calculate_totals();
+
+  ob_start();
+  fpusa_get_order_summary();
+  $fpusa_order_summary = ob_get_clean();
+
+	wp_send_json( $fpusa_order_summary );
+	wp_die();
+}
+
+add_action( 'fpusa_order_summary', 'fpusa_get_order_summary' );
+function fpusa_get_order_summary(){
+	$totals = WC()->cart->get_totals();
+	// var_dump( $totals );
+	?>
+	<div id="cart-totals">
+		<h5 class="my-3">Order Summary</h5>
+		<table class="table">
+			<?php
+				$formatted_totals = array(
+					array('Items', $totals['cart_contents_total']),
+					array('Shipping & handling', $totals['shipping_total']),
+					array('Your coupon savings', $totals['discount_total']),
+					array('Estimated tax to be collected', $totals['fee_total']),
+				);
+
+				foreach( $formatted_totals as $total_arr ){
+					echo fpusa_get_order_totals_html( $total_arr );
+				}
+			?>
+
+		<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
+
+			<tr class="fpusa-order-total">
+				<th><?php _e( 'Order total:', 'woocommerce' ); ?></th>
+				<td>$<?php echo $totals['total']; ?></td>
+			</tr>
+
+			<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
+		</table>
+	</div>
+	<?php
+}
 
 function compare_user_prefs(){
 	$prefs = get_user_order_prefs();
@@ -132,7 +194,7 @@ function fpusa_place_order(){
 		<div class="d-flex align-items-center">
 			<button id="place-order" type="submit" class="btn btn-warning btn-block">Place your order</button>
 			<span class="mx-5">
-				<div class="order-total">
+				<div class="fpusa-order-total">
 					Order Total: <?php wc_cart_totals_order_total_html(); ?>
 				</div>
 				<p id="order-instructions-btm"><?php echo wc_get_privacy_policy_text( 'checkout' ); ?></p>
@@ -209,25 +271,44 @@ function fpusa_choose_shipping_address(){
   <?php
 }
 
-add_action( 'wp_ajax_fpusa_get_billing_info', 'fpusa_get_billing_info' );
-function fpusa_get_billing_info(){
+add_action( 'wp_ajax_fpusa_get_billing_address', 'fpusa_get_billing_address' );
+function fpusa_get_billing_address( $id = '' ){
 	// echo $_POST['id'];
+	$doing_ajax = ( isset( $_POST['id'] ) );
+	$id = ( $doing_ajax ) ? $_POST['id'] : $id;
 
-		$billing = array(
-			'first_name' => get_metadata( 'payment_token', $_POST['id'], 'billing_first_name', true ),
-			'last_name' => get_metadata( 'payment_token', $_POST['id'], 'billing_last_name', true ),
-			'address_1' => get_metadata( 'payment_token', $_POST['id'], 'billing_address_1', true ),
-			'address_2' => get_metadata( 'payment_token', $_POST['id'], 'billing_address_2', true ),
-			'city' => get_metadata( 'payment_token', $_POST['id'], 'billing_city', true ),
-			'state' => get_metadata( 'payment_token', $_POST['id'], 'billing_state', true ),
-			'postcode' => get_metadata( 'payment_token', $_POST['id'], 'billing_postcode', true ),
-			'country' => get_metadata( 'payment_token', $_POST['id'], 'billing_country', true ),
-		);
+	$billing = array(
+		'billing_first_name' => get_metadata( 'payment_token', $id, 'billing_first_name', true ),
+		'billing_last_name' => get_metadata( 'payment_token', $id, 'billing_last_name', true ),
+		'billing_address_1' => get_metadata( 'payment_token', $id, 'billing_address_1', true ),
+		'billing_address_2' => get_metadata( 'payment_token', $id, 'billing_address_2', true ),
+		'billing_city' => get_metadata( 'payment_token', $id, 'billing_city', true ),
+		'billing_state' => get_metadata( 'payment_token', $id, 'billing_state', true ),
+		'billing_postcode' => get_metadata( 'payment_token', $id, 'billing_postcode', true ),
+		'billing_country' => get_metadata( 'payment_token', $id, 'billing_country', true ),
+	);
 
+	if( $doing_ajax ){
 		wp_send_json( $billing );
-
 		wp_die();
+	} else {
+		return $billing;
+	}
 
+}
+
+add_action( 'wp_ajax_fpusa_billing_address_edit', 'fpusa_billing_address_edit' );
+function fpusa_billing_address_edit(){
+		parse_str( $_POST['form_data'], $form_data );
+		foreach( $form_data as $key => $value ){
+			if( ! empty( $value ) ){
+				update_metadata( 'payment_token', $_POST['id'], $key, $value);
+				update_user_meta( get_current_user_id(), $key, $value );
+			}
+		}
+
+		wp_send_json( fpusa_get_billing_address( $_POST['id'] ) );
+		wp_die();
 }
 
 add_action( 'wp_ajax_fpusa_checkout_address', 'fpusa_get_checkout_address' );
@@ -260,10 +341,10 @@ function iww_make_date( $dates ){
     // if this isn't the first date, add a hyphen to the string
 		if( $key != 0 )$date_str .= ' - ';
     // create date based on leadtime + $date passed to function
-		$future = date( 'l, M j', strtotime('+' . $date . 'days') );
+		$future = date( 'l, F jS', strtotime('+' . $date . 'days') );
     // if future lands on a sunday, add another day to it.
-		if( preg_match( '/Saturday/', $future ) ) $future = date( 'l, F j', strtotime('+' . ($date + 2) . 'days') );
-		if( preg_match( '/Sunday/', $future ) ) $future = date( 'l, F j', strtotime('+' . ($date + 1) . 'days') );
+		if( preg_match( '/Saturday/', $future ) ) $future = date( 'l, F jS', strtotime('+' . ($date + 2) . 'days') );
+		if( preg_match( '/Sunday/', $future ) ) $future = date( 'l, F jS', strtotime('+' . ($date + 1) . 'days') );
 
 		$date_str .= $future;
 	}
@@ -322,8 +403,7 @@ function fpusa_get_order_totals_html( $arr ){
 	?>
 	<tr>
 		<td><?php echo $arr[0] ?>:</td>
-		<!--  TODO: integrate with js  -->
-		<td <?php if( $arr[0] == 'Shipping & handling' ) echo 'id="shipping_fees"';?> >$<?php echo $arr[1] ?></td>
+		<td>$<?php echo number_format( $arr[1], 2 ) ?></td>
 	</tr>
 	<?php
 }
@@ -347,50 +427,7 @@ function fpusa_get_customer_saved_methods_list( $user_id ){
 	return $token_arr;
 }
 
-add_action( 'wp_ajax_fpusa_get_coupon_html', 'fpusa_get_applied_coupons' );
-add_action( 'wp_ajax_nopriv_fpusa_get_coupon_html', 'fpusa_get_applied_coupons' );
 
-function fpusa_get_applied_coupons( $echo = true ){
-	$html = '';
-	$applied_coupons = WC()->cart->get_coupon_discount_totals();
-	if( ! empty( $applied_coupons ) ){
-		foreach( $applied_coupons as $code => $coupon ){
-			$html .= "<div class='d-flex coupon-line'>";
-			$html .= 	"<button id='$code' type='button' class='close pr-3' aria-label='Close'>";
-			$html .= 		"&times";
-			$html .= 	"</button>";
-			$html .= 	"<span class='p-0 m-0 font-weight-bold pr-3'>$code</span>";
-			$html .= 	"<span class='price'>$-". number_format( $coupon, 2 ) ."</span>";
-			$html .= "</div>";
-		}
-		if( $echo ){
-			echo $html;
-			// wp_die();
-		} else {
-			return $html;
-		}
-	}
-}
-
-add_action( 'wp_ajax_fpusa_apply_coupon', 'fpusa_apply_coupon' );
-add_action( 'wp_ajax_nopriv_fpusa_apply_coupon', 'fpusa_apply_coupon' );
-
-function fpusa_apply_coupon(){
-	if( WC()->cart->add_discount( sanitize_text_field( $_POST['code'] ) ) ){
-		echo 1;
-	} else {
-		wc_print_notices();
-	}
-  wp_die();
-}
-
-add_action( 'wp_ajax_fpusa_remove_coupon', 'fpusa_remove_coupon' );
-add_action( 'wp_ajax_nopriv_fpusa_remove_coupon', 'fpusa_remove_coupon' );
-function fpusa_remove_coupon(){
-	echo ( WC()->cart->remove_coupon( sanitize_text_field( $_POST['code'] ) ) );
-	wc_print_notices();
-	wp_die();
-}
 
 add_action( 'wp_ajax_fpusa_get_address', 'fpusa_ajax_get_address' );
 // add_action( 'wp_ajax_nopriv_fpusa_address_create', 'fpusa_ajax_address_create' );
@@ -433,4 +470,27 @@ function fpusa_ajax_address_edit(){
 
 
 	wp_die();
+}
+
+
+function fpusa_get_applied_coupons( $echo = true ){
+	$html = '';
+	$applied_coupons = WC()->cart->get_coupon_discount_totals();
+	if( ! empty( $applied_coupons ) ){
+		foreach( $applied_coupons as $code => $coupon ){
+			$html .= "<div class='d-flex coupon-line border-bottom pb-1'>";
+			$html .= 	"<button id='$code' type='button' data-coupon='$code' class='woocommerce-remove-coupon close pr-3' aria-label='Close'>";
+			$html .= 		"&times";
+			$html .= 	"</button>";
+			$html .= 	"<span class='p-0 m-0 font-weight-bold pr-3'>$code</span>";
+			$html .= 	"<span class='price'>$-". number_format( $coupon, 2 ) ."</span>";
+			$html .= "</div>";
+		}
+		if( $echo ){
+			echo $html;
+			// wp_die();
+		} else {
+			return $html;
+		}
+	}
 }
